132576f6da14cf47a9c61e62403640eba4895d78
MariaG <MGrigorenko@bellintegrator.com>
2022-11-24 01:28:38 +0300

    modified test back so that it runs successfully

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -15,6 +15,6 @@ public class PostmanEchoTest {
                     .post("/post")
                     .then()
                     .statusCode(200)
-                    .body("data", equalTo("another value"));
+                    .body("data", equalTo("some value"));
     }
 }
